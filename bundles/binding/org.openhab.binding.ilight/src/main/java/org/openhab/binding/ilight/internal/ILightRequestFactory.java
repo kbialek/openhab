bb035a11659bf2376faebe4992a510b1e5683079
Krzysztof Białek <krzysiek.bialek@gmail.com>
2015-06-30 21:38:35 +0200

    ilight: led color control

--- bundles/binding/org.openhab.binding.ilight/src/main/java/org/openhab/binding/ilight/internal/ILightRequestFactory.java
+++ bundles/binding/org.openhab.binding.ilight/src/main/java/org/openhab/binding/ilight/internal/ILightRequestFactory.java
@@ -1,6 +1,7 @@
 package org.openhab.binding.ilight.internal;
 
 import org.openhab.binding.ilight.ILightBindingProvider;
+import org.openhab.core.library.types.HSBType;
 import org.openhab.core.library.types.OnOffType;
 import org.openhab.core.types.Command;
 
@@ -11,13 +12,19 @@ public class ILightRequestFactory {
 		if (type == ILightBindingType.Triac) {
 			Integer out = provider.getOut(itemName);
 			return new byte[] { (byte) 1, out.byteValue(), commandToByte(command) };
-		} else if (type == ILightBindingType.Led) {
+		} else if (type == ILightBindingType.Led && command instanceof OnOffType) {
 			return new byte[] { (byte) 7, commandToByte(command) };
+		} else if (type == ILightBindingType.Led && command instanceof HSBType) {
+			HSBType hsb = (HSBType) command;
+			byte r = (byte) (hsb.getRed().intValue() / 10);
+			byte g = (byte) (hsb.getGreen().intValue() / 10);
+			byte b = (byte) (hsb.getBlue().intValue() / 10);
+			return new byte[] { (byte) 9, r, g, b };
 		} else {
 			return null;
 		}
 	}
-	
+
 	private static byte commandToByte(Command command) {
 		byte state = 0;
 		if (command == OnOffType.ON) {
